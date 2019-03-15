426334426982f8a19dc1002c24de59c933bd07a7
Chris Breaux <christopherjbreaux@gmail.com>
2019-03-15 10:01:48 -0700

    pass first spec

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -1,5 +1,6 @@
 
 import { test } from './../src/backEnd.js';
+import { birthday } from './../src/backEnd.js';
 
 
 
@@ -9,6 +10,17 @@ describe('test', function() {
   it('return a string of "working!"', function() {
 
     expect(test()).toEqual("working!");
+  });
+
+});
+
+
+describe('birthday', function() {
+
+  it('return an age using birth year as input', function() {
+    let birthYear = 1983;
+    let output = birthday(birthYear);
+    expect(output).toEqual(36);
   });
 
 });
--- src/backEnd.js
+++ src/backEnd.js
@@ -1,3 +1,12 @@
 export function test() {
   return 'working!'
 }
+
+export function birthday(year) {
+  const yearString = `${year}`;
+  const date = new Date();
+  const currentYear = date.getFullYear();
+  const age = currentYear - year;
+  return age;
+
+}
